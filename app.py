34bd57085e34455ccf6d64fead7600f1069391b0
Alef Rodrigues <alefbruno@Outlook.com>
2022-11-29 15:49:36 -0300

    Fix CSRF

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-  return "Laboratório Pipeline DevOps"
+  return "MBA Arquitetura e Soluções FIAP - Grupo 25"
 
 if __name__ == '__main__':
   app.run()
