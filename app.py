bf2e3af0eb8039b00795392f1662b940b5fef8a0
Alef Rodrigues <alefbruno@Outlook.com>
2023-04-21 14:46:29 -0300

    Incluindo o wtf, na instalação

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask
-import os
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-csrf = CSRFProtect(app)
+csrf = CSRFProtect(app)                                                                                                                           
 
 @app.route("/")
 def pagina_inicial():
-  return "MBA Arquitetura e Soluções FIAP - Grupo 25"
+    return "Laboratório Pipeline DevOps - vFinal"
 
 if __name__ == '__main__':
-    port = os.getenv('PORT')
-    app.run('0.0.0.0', port=port)
+    app.run()
