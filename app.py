a1d4fc2ad3cfdff93ae37d19b1c986021d4aa4f1
Alef Rodrigues <alefbruno@Outlook.com>
2023-04-21 14:48:31 -0300

    Ajustando Mensagem do Site

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-csrf = CSRFProtect(app)                                                                                                                           
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps - vFinal"
+    return "MBA Arquitetura e Soluções FIAP - Grupo 25"
 
 if __name__ == '__main__':
     app.run()
